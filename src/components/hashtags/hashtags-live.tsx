e9b5de8cdff71b785992d4d9fd30b4a58d91652f
Marius Muntean <marius.muntean@live.com>
2022-06-05 14:49:47 +0200

    Added a list with the hashtag ranks

--- src/components/hashtags/hashtags-live.tsx
+++ src/components/hashtags/hashtags-live.tsx
@@ -1,13 +1,14 @@
-import { useMemo } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import { useSubscription } from '@apollo/client'
-import { Button, Col, Row, Spin } from 'antd'
-import { DownloadOutlined } from '@ant-design/icons'
+import { Button, Col, Row, Spin, Table } from 'antd'
+import { DownloadOutlined, StopOutlined } from '@ant-design/icons'
 import Column from 'antd/lib/table/Column'
 import ReactWordcloud, { OptionsProp } from 'react-wordcloud'
 
 import { useGetHashtagsQuery, useHastagAddedSubscription, VisualizerQuery } from '../../generated/graphql'
 import { HashtagSubscription } from './subscriptions'
 import { startStreaming, stopStreaming } from '../../infrastructure/api/testApi'
+import { ColumnType } from 'antd/lib/table'
 
 interface Word {
     text: string
@@ -17,14 +18,15 @@ interface Word {
 export const HashtagsLive = () => {
     const { loading: loadingHashtags, data: hashtagsData } = useGetHashtagsQuery({ variables: { amount: 120 } })
     const { loading: loadingHashtagAdded, data: hashtagAddedData } = useHastagAddedSubscription()
+    const [wordCloudData, setWordCloudData] = useState<Word[]>([])
 
     // ToDo: add buttons to start and stop streamming
     // ToDo: show ranked list of hashtag names and scores
     // ToDo: throttle the updates somehow. The wordcloud is to active.
 
-    const wordCloudData = useMemo(() => {
-        if (loadingHashtags || loadingHashtagAdded) {
-            return []
+    useEffect(() => {
+        if (loadingHashtags) {
+            return
         }
 
         // map hashtags to Word[]
@@ -34,23 +36,35 @@ export const HashtagsLive = () => {
                 return w
             }) ?? []
 
+        setWordCloudData(words)
+    }, [hashtagsData?.hashtag?.topHashtags])
+
+    useEffect(() => {
+        if (loadingHashtags || loadingHashtagAdded || wordCloudData.length < 1) {
+            return
+        }
+
+        // map hashtags to Word[]
+        let words = wordCloudData
+
         // Add new hashtags to the Word[]
         const newHashtag = hashtagAddedData?.hashtagAdded
         if (!newHashtag) {
-            return words
+            return
         }
         const newHashtagIndex = words.findIndex((w) => w.text === newHashtag?.name)
         if (newHashtagIndex === -1) {
             words = [...words, { text: newHashtag.name!, value: newHashtag.score }]
         } else {
             const currentWord = words[newHashtagIndex]
-            currentWord.value = currentWord.value + 1
+            currentWord.value = newHashtag.score
             words[newHashtagIndex] = currentWord
         }
-        return words
-    }, [hashtagsData?.hashtag?.topHashtags, hashtagAddedData?.hashtagAdded?.name, hashtagAddedData?.hashtagAdded?.score])
+        console.log(words[0])
+        setWordCloudData([...words])
+    }, [hashtagAddedData?.hashtagAdded?.name, hashtagAddedData?.hashtagAdded?.score])
 
-    if (loadingHashtags || loadingHashtagAdded) {
+    if (loadingHashtags) {
         return <Spin size="default"></Spin>
     }
 
@@ -71,6 +85,17 @@ export const HashtagsLive = () => {
     }
     const size: [number, number] = [900, 600]
 
+    const columns: ColumnType<Word>[] = [
+        {
+            title: 'Hashtag',
+            render: (val, record) => record.text,
+        },
+        {
+            title: 'Rank',
+            render: (val, record) => record.value,
+        },
+    ]
+
     return (
         <>
             <Row>
@@ -81,14 +106,25 @@ export const HashtagsLive = () => {
                     </Button>
                 </Col>
                 <Col span={6} style={{ display: 'flex', justifyContent: 'center' }}>
-                    <Button type="primary" icon={<DownloadOutlined />} size={'large'} onClick={stopStreaming}>
+                    <Button type="primary" icon={<StopOutlined />} size={'large'} onClick={stopStreaming}>
                         Stop Streaming
                     </Button>
                 </Col>
                 <Col span={6}></Col>
             </Row>
             <Row>
-                <Col span={24}>
+                <Col span={4}>
+                    <Table
+                        dataSource={[
+                            ...wordCloudData.sort(function (a, b) {
+                                return b.value - a.value
+                            }),
+                        ]}
+                        columns={columns}
+                        rowKey={(r) => r.text}
+                    ></Table>
+                </Col>
+                <Col span={20}>
                     <ReactWordcloud words={wordCloudData} options={options} />
                 </Col>
             </Row>
